d021ec190eb2a23663beabead02e421c0604f8c0
Kevin Beeler <kevinbeeler@kevins-mbp.lan>
2023-03-01 17:00:29 -0500

    session added

--- app.js
+++ app.js
@@ -1,20 +1,40 @@
+//APP.JS in ~/app.js
 const express = require('express');
+const mongoose = require('mongoose');
 const router = express.Router();
 const app = express();
-const mongoose = require('mongoose');
 const expressEjsLayout = require('express-ejs-layouts')
+const flash = require('connect-flash');
+const session = require('express-session');
 //mongoose
 mongoose.connect('mongodb://localhost/test',{useNewUrlParser: true, useUnifiedTopology : true})
 .then(() => console.log('connected,,'))
 .catch((err)=> console.log(err));
+
 //EJS
 app.set('view engine','ejs');
 app.use(expressEjsLayout);
 //BodyParser
 app.use(express.urlencoded({extended : false}));
+//express session
+app.use(session({
+    secret : 'secret',
+    resave : true,
+    saveUninitialized : true
+   }));
+   //use flash
+   app.use(flash());
+   app.use((req,res,next)=> {
+     res.locals.success_msg = req.flash('success_msg');
+     res.locals.error_msg = req.flash('error_msg');
+     res.locals.error  = req.flash('error');
+   next();
+   })
 
 //Routes
 app.use('/',require('./routes/index'));
+
+
 app.use('/users',require('./routes/users'));
 
-app.listen(3000);
+app.listen(3000); 
